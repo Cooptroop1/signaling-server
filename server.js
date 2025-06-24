3a360f8e97d9ab44c1db3cd09497635489cdfae6
Cooptroop1 <jcooper4888@aol.co.uk>
2025-06-25 00:02:50 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+console.log('Signaling server running on port', process.env.PORT || 8080);

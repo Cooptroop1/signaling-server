9b8f904c99c129f504c6cdafbff3d8836029886d
Cooptroop1 <jcooper4888@aol.co.uk>
2025-08-16 15:48:40 +0100

    Update events.js

--- events.js
+++ events.js
@@ -38,7 +38,23 @@ let signingKey;
 let remoteAudios = new Map();
 let refreshingToken = false;
 let signalingQueue = new Map();
+let connectedClients = new Set();
 let socket = new WebSocket('wss://signaling-server-zc6m.onrender.com');
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+}
+function setCookie(name, value, days) {
+  let expires = '';
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/; Secure; HttpOnly; SameSite=Strict';
+}
 if (getCookie('clientId')) {
   clientId = getCookie('clientId');
 } else {

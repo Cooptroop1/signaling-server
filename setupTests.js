39b75e8cd1254e52d11d35efadea56e8398abd0f
Cooptroop1 <jcooper4888@aol.co.uk>
2025-09-02 13:02:11 +0100

    Update setupTests.js

--- setupTests.js
+++ setupTests.js
@@ -2,15 +2,28 @@
 global.window = global;
 global.window.crypto = {
   subtle: {
-    generateKey: jest.fn(),
-    importKey: jest.fn(),
-    exportKey: jest.fn(),
-    deriveKey: jest.fn(),
-    deriveBits: jest.fn(),
-    encrypt: jest.fn(),
-    decrypt: jest.fn(),
-    sign: jest.fn(),
-    verify: jest.fn(),
+    generateKey: jest.fn().mockImplementation(async (algorithm, extractable, usages) => ({
+      privateKey: { algorithm, extractable, usages },
+      publicKey: { algorithm, extractable, usages },
+    })),
+    importKey: jest.fn().mockImplementation(async (format, keyData, algorithm, extractable, usages) => ({
+      format,
+      keyData,
+      algorithm,
+      extractable,
+      usages,
+    })),
+    exportKey: jest.fn().mockImplementation(async (format, key) => new Uint8Array(96).buffer),
+    deriveKey: jest.fn().mockImplementation(async (algorithm, baseKey, derivedKeyAlgorithm, extractable, usages) => ({
+      algorithm: derivedKeyAlgorithm,
+      extractable,
+      usages,
+    })),
+    deriveBits: jest.fn().mockImplementation(async (algorithm, baseKey, length) => new Uint8Array(length / 8).buffer),
+    encrypt: jest.fn().mockImplementation(async (algorithm, key, data) => new Uint8Array(data).buffer),
+    decrypt: jest.fn().mockImplementation(async (algorithm, key, data) => new TextEncoder().encode('decrypted').buffer),
+    sign: jest.fn().mockImplementation(async (algorithm, key, data) => new Uint8Array(32).buffer),
+    verify: jest.fn().mockImplementation(async (algorithm, key, signature, data) => true),
   },
   getRandomValues: jest.fn(arr => arr),
   randomUUID: jest.fn(() => 'mock-uuid'),
@@ -34,7 +47,7 @@ global.WebSocket = class {
   }
 };
 global.RTCPeerConnection = jest.fn(() => ({
-  createOffer: jest.fn(() => Promise.resolve({})),
+  createOffer: jest.fn(() => Promise.resolve({ type: 'offer' })),
   setLocalDescription: jest.fn(() => Promise.resolve()),
   setRemoteDescription: jest.fn(() => Promise.resolve()),
   createAnswer: jest.fn(() => Promise.resolve({ type: 'answer' })),
@@ -71,14 +84,28 @@ global.document = {
     innerHTML: '',
     setAttribute: jest.fn(),
     textContent: '',
+    style: {},
+    controls: false,
+    src: '',
+    dataset: {},
+    title: '',
+    value: '',
+    onclick: null,
   })),
-  getElementById: jest.fn(() => ({
+  getElementById: jest.fn(id => ({
     classList: { add: jest.fn(), remove: jest.fn(), toggle: jest.fn() },
     appendChild: jest.fn(),
     focus: jest.fn(),
     innerHTML: '',
     setAttribute: jest.fn(),
     textContent: '',
+    style: {},
+    controls: false,
+    src: '',
+    dataset: {},
+    title: '',
+    value: '',
+    onclick: null,
   })),
   querySelectorAll: jest.fn(() => []),
   cookie: '',
@@ -93,3 +120,10 @@ global.navigator = {
 };
 global.performance = { now: jest.fn(() => Date.now()) };
 global.window.location = { reload: jest.fn(), href: '', search: '' };
+global.window.DOMPurify = { sanitize: jest.fn(content => content) };
+global.window.otplib = {
+  authenticator: {
+    generateSecret: jest.fn(() => 'JBSWY3DPEHPK3PXP'),
+    keyuri: jest.fn(() => 'otpauth://totp/Anonomoose%20Chat:test-room?secret=JBSWY3DPEHPK3PXP'),
+  },
+};

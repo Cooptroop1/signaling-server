449b062e2d305028484d26c01a3eca310b778e66
Cooptroop1 <jcooper4888@aol.co.uk>
2026-01-12 22:30:29 +0000

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,11 +1,8 @@
 // This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "offline.html";  // Replace with your file
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -16,7 +13,15 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll([
+        offlineFallbackPage,
+        '/',  // index.html
+        '/crypto.js',
+        '/utils.js',
+        '/main.js',
+        '/init.js',
+        '/events.js'
+      ]))
   );
 });
 
@@ -29,15 +34,12 @@ self.addEventListener('fetch', (event) => {
     event.respondWith((async () => {
       try {
         const preloadResp = await event.preloadResponse;
-
         if (preloadResp) {
           return preloadResp;
         }
-
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
-
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;

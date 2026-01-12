e9f40aace7676aa5c55bdd1a0ec24ae00bfec847
Cooptroop1 <jcooper4888@aol.co.uk>
2026-01-12 22:57:19 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,50 @@
+
+// This is the "Offline page" service worker
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+const offlineFallbackPage = "offline.html";  // Replace with your file
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll([
+        offlineFallbackPage,
+        '/',  // index.html
+        '/crypto.js',
+        '/utils.js',
+        '/main.js',
+        '/init.js',
+        '/events.js'
+      ]))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+        if (preloadResp) {
+          return preloadResp;
+        }
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
